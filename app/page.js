ef9aacd070fa48091e531c8d42f4373ce96569f6
Nouriokart <boulhaian@coventry.ac.uk>
2025-06-14 13:45:13 +0100

    Initial commit: Setup Next.js plant tracker

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <main style={{ padding: '2rem' }}>
+        <h1>Plant Care Assistant</h1>
+        <p>Welcome to your plant tracker!</p>
+      </main>
+    );
+  }
---
+++ plant-tracker-nextjs/app/test/page.js
@@ -0,0 +1,3 @@
+﻿export default function Test() {
+  return <div>Hello World</div>;
+}
